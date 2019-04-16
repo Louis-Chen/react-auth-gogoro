945f9e43872f35c66b950c3ef51d61a99f9e36f7
LuisChen <leo5916267@gmail.com>
2019-04-17 00:00:18 +0800

    完成綁定手機驗證

--- src/components/user/Phone.js
+++ src/components/user/Phone.js
@@ -1,131 +1,60 @@
-import React, { Component, useState } from 'react'
-import { compose } from 'recompose'
-import { Form, Input, Button } from 'semantic-ui-react'
+import React, { useState } from 'react'
+import { compose, withHandlers, withState } from 'recompose'
+import { Form, Input, Button, Message } from 'semantic-ui-react'
 import { withFirebase } from 'react-redux-firebase'
 
-class UserPhone extends Component {
-	constructor(props) {
-		super(props)
-		this.handleSubmit = this.handleSubmit.bind(this)
-		this.handleChange = this.handleChange.bind(this)
-		this.handleVerification = this.handleVerification.bind(this)
-		this.state = {
-			phoneNumber: '',
-			captchaRespone: '',
-			showVerification: false,
-			verificationCode: ''
-		}
-	}
-	handleSubmit(e) {
-		const { firebase } = this.props
-		e.preventDefault()
-		//	const phone ='+12345678000'
-		const { phoneNumber } = this.state
-		if (this.state.captchaRespone.length > 0) {
-			let appVerifier = window.recaptchaVerifier
-			firebase
-				.signInWithPhoneNumber(phoneNumber, appVerifier)
-				.then(confirmResult => {
-					// This means that the SMS has been sent to the user
-					// You need to:
-					//   1) Save the `confirmResult` object to use later
-					this.setState({ confirmResult, showVerification: true })
-					//   2) Hide the phone number form
-					//   3) Show the verification code form
-				})
-				.catch(error => {
-					const { code, message } = error
-					// For details of error codes, see the docs
-					// The message contains the default Firebase string
-					// representation of the error
-				})
-		}
-	}
-	handleChange(e) {
-		this.setState({
-			[e.target.name]: e.target.value
-		})
-	}
-	handleVerification(e) {
-		e.preventDefault()
-		const { confirmResult, verificationCode } = this.state
-		confirmResult
-			.confirm(verificationCode)
-			.then(user => {
-				// If you need to do anything with the user, do it here
-				// The user will be logged in automatically by the
-				// `onAuthStateChanged` listener we set up in App.js earlier
-				console.log(user)
-				this.setState({
-					verificationCode: ''
-				})
-			})
-			.catch(error => {
-				const { code, message } = error
-				// For details of error codes, see the docs
-				// The message contains the default Firebase string
-				// representation of the error
-			})
-	}
-	componentDidMount() {
-		const { firebase } = this.props
-
-		window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('sign-in-button', {
-			size: 'normal',
-			callback: response => {
-				// reCAPTCHA solved, allow signInWithPhoneNumber.
-				// ...
-				console.log(response)
-				this.setState({
-					captchaRespone: response
-				})
-			},
-			'expired-callback': function() {
-				// Response expired. Ask user to solve reCAPTCHA again.
-				// ...
-			}
-		})
-		window.recaptchaVerifier.render().then(function(widgetId) {
-			window.recaptchaWidgetId = widgetId
-		})
-	}
-	render() {
-		return (
-			<Form>
-				{!this.state.showVerification && (
-					<Form.Field>
-						<label>手機驗證</label>
-						<Input
-							name="phoneNumber"
-							autoFocus={true}
-							onChange={this.handleChange}
-							type="text"
-							value={this.state.phoneNumber}
-							autoComplete="off"
-						/>
-						<Button onClick={this.handleSubmit} type="submit" content="送出" />
-						<div id="sign-in-button" />
-					</Form.Field>
-				)}
-				{this.state.showVerification && (
-					<Form.Field>
-						<label>手機驗證</label>
-						<div id="sign-in-button" />
-						<Input
-							name="verificationCode"
-							onChange={this.handleChange}
-							type="text"
-							value={this.state.verificationCode}
-							autoComplete="off"
-							autoFocus={true}
-						/>
-						<Button onClick={this.handleVerification} type="submit" content="驗證" />
-					</Form.Field>
-				)}
-			</Form>
-		)
-	}
+const UserPhone = props => {
+	const { signInPhone, message, setVerifyCode, verifyCode, confirmCode } = props
+	const [phone, setPhone] = useState('')
+	return (
+		<Form>
+			<Form.Field>
+				<label>手機號碼</label>
+				<Input name="phone" value={phone} onChange={(e, { name, value }) => setPhone(value)} />
+				<Button type="button" onClick={() => signInPhone(phone)} content="送出驗證碼" />
+				<div id="recaptcha-container" />
+			</Form.Field>
+			<Form.Field>
+				<label>驗證碼</label>
+				<input onChange={e => setVerifyCode(e.target.value)} name="verifyCode" value={verifyCode} />
+				<Button type="button" onClick={() => confirmCode()} content="驗證手機" />
+			</Form.Field>
+			<Message content={message} />
+		</Form>
+	)
 }
-
-const enhancer = compose(withFirebase)
+const enhancer = compose(
+	withFirebase,
+	withState('message', 'setMessage', ''),
+	withState('confirmResult', 'setConfirmResult', null),
+	withState('verifyCode', 'setVerifyCode', ''),
+	withHandlers({
+		signInPhone: ({ firebase, setMessage, setConfirmResult }) => phone => {
+			console.log(phone)
+			const appVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container')
+			firebase.auth().useDeviceLanguage()
+			firebase
+				.auth()
+				.signInWithPhoneNumber(phone, appVerifier)
+				.then(confirmResult => {
+					console.log(confirmResult)
+					setConfirmResult(confirmResult)
+				})
+				.then(() => setMessage('成功送出驗證'))
+				.catch(error => setMessage(`Sign In With Phone Number Error: ${error.message}`))
+		},
+		confirmCode: ({ confirmResult, verifyCode, setMessage, firebase }) => e => {
+			if (confirmResult && verifyCode.length) {
+				const credential = firebase.auth.PhoneAuthProvider.credential(confirmResult.verificationId, verifyCode)
+				firebase
+					.auth()
+					.currentUser.linkAndRetrieveDataWithCredential(credential)
+					.then(user => {
+						setMessage('Code Confirmed!')
+					})
+					.catch(error => setMessage(`Code Confirm Error: ${error.message}`))
+			}
+		}
+	})
+)
 export default enhancer(UserPhone)
